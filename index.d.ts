a00abc59de97ea567efe58e8ee63f3b484c8ac20
Amir Maghami <Maghami.A1987@gmail.com>
2023-04-26 00:28:56 +0430

    Update index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import { DefineComponent } from 'vue'
+import VueCuteButton from "./src/VueCuteButton.vue";
+export declare const VueCuteButton: DefineComponent<{
+  btnStyle?: string
+}>
+
+export default VueCuteButton
