9160b5674be392909f620e54de618b0e9e82c29f
Amir Maghami <Maghami.A1987@gmail.com>
2023-09-25 15:19:58 +0330

    fix: style and props

--- src/VueCuteButton.vue
+++ src/VueCuteButton.vue
@@ -1,43 +1,31 @@
 <template>
-  <div class="btn">
-    <a href="#" :class="`anim-${props.btnStyle}`">
-      <span
-        v-if="
-          props.btnStyle == 6 ||
-          props.btnStyle == 7 ||
-          props.btnStyle == 11 ||
-          props.btnStyle == 12
-        "
-        ><slot></slot
-      ></span>
+  <template v-if="props.use == 'nuxt-link'">
+    <nuxt-link :to="props.url" :class="`anim-${props.btnStyle}`">
       <slot></slot>
-    </a>
-    <!-- <a href="#" v-if="props.btnStyle == '2'" class="anim-2">Hover Me</a>
-    <a href="#" v-if="props.btnStyle == '3'" class="anim-3">Hover Me</a>
-    <a href="#" v-if="props.btnStyle == '4'" class="anim-4">Hover Me</a>
-    <a href="#" v-if="props.btnStyle == '5'" class="anim-5">Hover Me</a>
-    <a href="#" v-if="props.btnStyle == '6'" class="anim-6"><span>Hover Me</span></a>
-    <a href="#" v-if="props.btnStyle == '7'" class="anim-7"><span>Hover Me</span></a>
-    <a href="#" v-if="props.btnStyle == '8'" class="anim-8">Hover Me</a>
-    <a href="#" v-if="props.btnStyle == '9'" class="anim-9">Hover Me</a>
-    <a href="#" v-if="props.btnStyle == '10'" class="anim-10"><span>H</span>over Me</a>
-    <a href="#" v-if="props.btnStyle == '11'" class="anim-11">Hover Me <span></span></a>
-    <a href="#" v-if="props.btnStyle == '12'" class="anim-12">Hover Me <span></span></a>
-    <a href="#" v-if="props.btnStyle == '13'" class="anim-13">Hover Me</a>
-    <a href="#" v-if="props.btnStyle == '14'" class="anim-14">Hover Me</a>
-    <a href="#" v-if="props.btnStyle == '15'" class="anim-15">Hover Me</a> -->
-  </div>
+    </nuxt-link>
+  </template>
+  <template v-else-if="props.use == 'vue-link'">
+    <router-link :to="props.url" :class="`anim-${props.btnStyle}`">
+      <slot></slot>
+    </router-link>
+  </template>
+  <template v-else>
+    <a :href="props.url" :class="`anim-${props.btnStyle}`"><slot></slot></a>
+  </template>
 </template>
 
 <script setup lang="ts">
 interface Props {
   btnStyle: string;
+  url: string;
+  use: "nuxt-link" | "vue-link" | "html-link";
 }
 const props = withDefaults(defineProps<Props>(), {
   btnStyle: "1",
+  url: "#",
+  use: "html-link",
 });
 </script>
-
 <style>
 .anim-1 {
   width: 12rem;
@@ -52,6 +40,7 @@ const props = withDefaults(defineProps<Props>(), {
   overflow: hidden;
   font-size: 1.4rem;
   font-family: sans-serif;
+  cursor: pointer;
 }
 .anim-1:hover {
   color: white;
@@ -268,176 +257,6 @@ const props = withDefaults(defineProps<Props>(), {
 }
 
 .anim-6 {
-  letter-spacing: 6px;
-  font-size: 1.4rem;
-  font-weight: bold;
-  color: #4a0c52;
-  text-transform: uppercase;
-  width: 12rem;
-  aspect-ratio: 3/1;
-  text-decoration: none;
-  overflow: hidden;
-  position: relative;
-  border: 3px solid #9a1aaa;
-  font-size: 1.2rem;
-  font-family: sans-serif;
-  transition: all 0.4s;
-}
-.anim-6:hover {
-  color: #fff;
-}
-.anim-6:hover:before {
-  left: 0;
-}
-.anim-6:hover:after {
-  top: 0;
-}
-.anim-6:before {
-  content: "";
-  position: absolute;
-  width: 25%;
-  height: 100%;
-  background-color: #9a1aaa;
-  z-index: -1;
-  transition: all 0.2s;
-  top: 0;
-  left: -25%;
-}
-.anim-6:after {
-  content: "";
-  position: absolute;
-  width: 25%;
-  height: 100%;
-  background-color: #9a1aaa;
-  z-index: -1;
-  transition: all 0.2s;
-  top: -100%;
-  left: 25%;
-}
-.anim-6 span {
-  height: 100%;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.anim-6 span:hover:before {
-  right: 0;
-}
-.anim-6 span:hover:after {
-  bottom: 0;
-}
-.anim-6 span:before {
-  content: "";
-  position: absolute;
-  width: 25%;
-  height: 100%;
-  background-color: #9a1aaa;
-  z-index: -1;
-  transition: all 0.2s;
-  top: 0;
-  right: -25%;
-}
-.anim-6 span:after {
-  content: "";
-  position: absolute;
-  width: 25%;
-  height: 100%;
-  background-color: #9a1aaa;
-  z-index: -1;
-  transition: all 0.2s;
-  bottom: -100%;
-  right: 25%;
-}
-
-.anim-7 {
-  letter-spacing: 6px;
-  font-size: 1.4rem;
-  font-weight: bold;
-  color: #4a0c52;
-  text-transform: uppercase;
-  width: 12rem;
-  aspect-ratio: 3/1;
-  text-decoration: none;
-  overflow: hidden;
-  position: relative;
-  border: 3px solid #9a1aaa;
-  font-size: 1.2rem;
-  font-family: sans-serif;
-  transition: all 0.1s;
-}
-.anim-7:hover {
-  color: #fff;
-}
-.anim-7:hover:before {
-  top: 0;
-}
-.anim-7:hover:after {
-  top: 0;
-}
-.anim-7:before {
-  content: "";
-  position: absolute;
-  width: 25%;
-  height: 100%;
-  background-color: #9a1aaa;
-  z-index: -1;
-  transition: all 0.2s;
-  top: 100%;
-  left: 0%;
-  transition-delay: 0.2s;
-}
-.anim-7:after {
-  content: "";
-  position: absolute;
-  width: 25%;
-  height: 100%;
-  background-color: #9a1aaa;
-  z-index: -1;
-  transition: all 0.2s;
-  top: -100%;
-  left: 25%;
-  transition-delay: 0.4s;
-}
-.anim-7 span {
-  height: 100%;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.anim-7 span:hover:before {
-  top: 0;
-}
-.anim-7 span:hover:after {
-  bottom: 0;
-}
-.anim-7 span:before {
-  content: "";
-  position: absolute;
-  width: 25%;
-  height: 100%;
-  background-color: #9a1aaa;
-  z-index: -1;
-  transition: all 0.2s;
-  top: -100%;
-  right: 0%;
-  transition-delay: 0.8s;
-}
-.anim-7 span:after {
-  content: "";
-  position: absolute;
-  width: 25%;
-  height: 100%;
-  background-color: #9a1aaa;
-  z-index: -1;
-  transition: all 0.2s;
-  bottom: -100%;
-  right: 25%;
-  transition-delay: 0.6s;
-}
-
-.anim-8 {
   text-decoration: none;
   text-transform: uppercase;
   color: #9a1aaa;
@@ -453,7 +272,7 @@ const props = withDefaults(defineProps<Props>(), {
   transition: all 0.1s;
   transition-duration: 1s;
 }
-.anim-8:before {
+.anim-6:before {
   content: "";
   border-top: 2px solid #9a1aaa;
   border-bottom: 2px solid green;
@@ -467,7 +286,7 @@ const props = withDefaults(defineProps<Props>(), {
   margin-bottom: auto;
   transition: all 0.4s;
 }
-.anim-8:after {
+.anim-6:after {
   content: "";
   border-left: 2px solid #9a1aaa;
   border-right: 2px solid #4a0c52;
@@ -481,18 +300,18 @@ const props = withDefaults(defineProps<Props>(), {
   margin-bottom: auto;
   transition: all 0.4s 0.2s;
 }
-.anim-8:hover {
+.anim-6:hover {
   color: green;
   font-weight: bold;
 }
-.anim-8:hover:before {
+.anim-6:hover:before {
   transform: rotateX(180deg);
 }
 .anim-8:hover:after {
   transform: rotateY(180deg);
 }
 
-.anim-9 {
+.anim-7 {
   text-decoration: none;
   text-transform: uppercase;
   color: #fff;
@@ -507,7 +326,7 @@ const props = withDefaults(defineProps<Props>(), {
   align-items: center;
   position: relative;
 }
-.anim-9:before {
+.anim-7:before {
   content: "";
   border-left: 2px solid #9a1aaa;
   border-top: 2px solid #9a1aaa;
@@ -518,7 +337,7 @@ const props = withDefaults(defineProps<Props>(), {
   top: -4px;
   transition: all 0.4s;
 }
-.anim-9:after {
+.anim-7:after {
   content: "";
   border-right: 2px solid #9a1aaa;
   border-bottom: 2px solid #9a1aaa;
@@ -529,170 +348,16 @@ const props = withDefaults(defineProps<Props>(), {
   bottom: -4px;
   transition: all 0.4s;
 }
-.anim-9:hover:before {
+.anim-7:hover:before {
   width: 90%;
   height: 90%;
 }
-.anim-9:hover:after {
+.anim-7:hover:after {
   width: 90%;
   height: 90%;
 }
 
-.anim-10 {
-  text-decoration: none;
-  color: #9a1aaa;
-  border: 2px solid #9a1aaa;
-  letter-spacing: 6px;
-  font-size: 1.2rem;
-  font-family: sans-serif;
-  width: 12rem;
-  aspect-ratio: 3/1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-}
-.anim-10:before {
-  content: "BB";
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  transition: all 2s;
-}
-.anim-10 span {
-  transition: all 2s;
-  transform-origin: center top;
-}
-.anim-10:hover {
-  color: #fff;
-}
-.anim-10:hover:before {
-  content: "BB";
-  background-color: #4a0c52;
-}
-.anim-10:hover span {
-  transform: rotate(-360deg);
-}
-
-.anim-11 {
-  text-decoration: none;
-  color: #9a1aaa;
-  letter-spacing: 6px;
-  font-size: 1.2rem;
-  font-family: sans-serif;
-  width: 12rem;
-  aspect-ratio: 3/1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  overflow: hidden;
-}
-.anim-11:before {
-  content: "";
-  border-left: 2px solid #9a1aaa;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 0px;
-  height: 0px;
-  transition: all 0.2s;
-}
-.anim-11:after {
-  content: "";
-  border-top: 2px solid #9a1aaa;
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 0px;
-  height: 0px;
-  transition: all 0.2s;
-}
-.anim-11 span:before {
-  content: "";
-  border-right: 2px solid #9a1aaa;
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  width: 0px;
-  height: 0px;
-  transition: all 0.2s;
-}
-.anim-11 span:after {
-  content: "";
-  border-bottom: 2px solid #9a1aaa;
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  width: 0px;
-  height: 0px;
-  transition: all 0.2s;
-}
-.anim-11:hover:before {
-  width: 100%;
-  height: 100%;
-}
-.anim-11:hover:after {
-  width: 100%;
-  height: 100%;
-}
-.anim-11:hover span:before {
-  width: 100%;
-  height: 100%;
-}
-.anim-11:hover span:after {
-  width: 100%;
-  height: 100%;
-}
-
-.anim-12 {
-  text-decoration: none;
-  color: #9a1aaa;
-  letter-spacing: 6px;
-  font-size: 1.2rem;
-  font-family: sans-serif;
-  width: 12rem;
-  aspect-ratio: 3/1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  overflow: hidden;
-}
-.anim-12 span {
-  z-index: -1;
-  position: absolute;
-  background-color: transparent;
-  border: 2px solid #9a1aaa;
-  left: 0;
-  top: 0;
-  width: calc(100% - 4px);
-  height: calc(100% - 4px);
-  transition: all 0.4s;
-}
-.anim-12 span:before {
-  content: "";
-  width: 8%;
-  height: 500%;
-  background-color: white;
-  position: absolute;
-  left: 50%;
-  right: 50%;
-  transform: translate(-50%, -50%) rotate(-60deg);
-  transition: all 0.3s;
-}
-.anim-12:hover {
-  color: white;
-}
-.anim-12:hover span:before {
-  width: 120%;
-  background-color: #9a1aaa;
-  transform: translate(-50%, -50%) rotate(60deg);
-}
-
-.anim-13 {
+.anim-8 {
   text-decoration: none;
   color: white;
   letter-spacing: 6px;
@@ -707,7 +372,7 @@ const props = withDefaults(defineProps<Props>(), {
   background: transparent;
   border: none;
 }
-.anim-13:before {
+.anim-8:before {
   content: "";
   position: absolute;
   width: 50%;
@@ -719,7 +384,7 @@ const props = withDefaults(defineProps<Props>(), {
   transform: skewx(30deg);
   transition: all 0.2s cubic-bezier(0.68, -0.55, 0.265, 1.55);
 }
-.anim-13:after {
+.anim-8:after {
   content: "";
   position: absolute;
   width: 50%;
@@ -731,20 +396,20 @@ const props = withDefaults(defineProps<Props>(), {
   transform: skewx(30deg);
   transition: all 0.2s cubic-bezier(0.68, -0.55, 0.265, 1.55);
 }
-.anim-13:hover:before {
+.anim-8:hover:before {
   left: 50%;
   top: 0;
   transform: skewx(0deg);
   background-color: blue;
 }
-.anim-13:hover:after {
+.anim-8:hover:after {
   right: 50%;
   top: 0;
   transform: skewx(0deg);
   background-color: red;
 }
 
-.anim-14 {
+.anim-9 {
   text-decoration: none;
   color: white;
   background-color: #c221d6;
@@ -760,7 +425,7 @@ const props = withDefaults(defineProps<Props>(), {
   border-radius: 2rem;
   letter-spacing: 6px;
 }
-.anim-14:before {
+.anim-9:before {
   content: "";
   position: absolute;
   left: 0;
@@ -772,11 +437,11 @@ const props = withDefaults(defineProps<Props>(), {
   border-radius: inherit;
   z-index: -1;
 }
-.anim-14:hover {
+.anim-9:hover {
   transform: translateY(-3px);
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
 }
-.anim-14:hover:before {
+.anim-9:hover:before {
   transform: scaleX(1.4) scaleY(1.6);
   opacity: 0;
 }
@@ -785,7 +450,7 @@ const props = withDefaults(defineProps<Props>(), {
   box-shadow: 0 5px 7px rgba(0, 0, 0, 0.2);
 }
 
-.anim-14 {
+.anim-9 {
   text-decoration: none;
   color: white;
   background-color: #c221d6;
@@ -801,7 +466,7 @@ const props = withDefaults(defineProps<Props>(), {
   border-radius: 2rem;
   letter-spacing: 6px;
 }
-.anim-14:before {
+.anim-9:before {
   content: "";
   position: absolute;
   left: 0;
@@ -814,7 +479,7 @@ const props = withDefaults(defineProps<Props>(), {
   z-index: -1;
 }
 
-.anim-15 {
+.anim-10 {
   text-decoration: none;
   color: black;
   background-color: transparent;
@@ -832,7 +497,7 @@ const props = withDefaults(defineProps<Props>(), {
   border-radius: 2rem;
   letter-spacing: 6px;
 }
-.anim-15:before {
+.anim-10:before {
   content: "";
   position: absolute;
   left: 0;
@@ -846,7 +511,7 @@ const props = withDefaults(defineProps<Props>(), {
   transform: translateX(-100%);
   transition: all 0.7s;
 }
-.anim-15:after {
+.anim-10:after {
   content: "";
   position: absolute;
   left: 0;
@@ -860,10 +525,10 @@ const props = withDefaults(defineProps<Props>(), {
   transform: translateX(100%);
   transition: all 0.7s;
 }
-.anim-15:hover:before {
+.anim-10:hover:before {
   transform: translateX(0);
 }
-.anim-15:hover:after {
+.anim-10:hover:after {
   transform: translateX(0);
 }
 </style>
